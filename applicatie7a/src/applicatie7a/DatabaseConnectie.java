10d78304f0bd233fe9fa2e84addf6563dfae7995
naam <marijnspr@gmail.com>
2017-03-30 16:53:34 +0200

    Eerste resultaten worden in de database gezet

--- applicatie7a/src/applicatie7a/DatabaseConnectie.java
+++ applicatie7a/src/applicatie7a/DatabaseConnectie.java
@@ -1,12 +1,18 @@
 package applicatie7a;
 
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Michelle on 29-3-2017.
  */
 public class DatabaseConnectie {
-    public void connectie() throws SQLException {
+    public void connectie(String header,ArrayList<ORF> forward, ArrayList<ORF> reverse, String refseq) throws SQLException {
         System.out.println("-------- Oracle JDBC Connection Testing ------");
 
         try {
@@ -41,18 +47,46 @@ public class DatabaseConnectie {
         if (connection != null) {
             System.out.println("You made it, take control your database now!");
             Statement stmt = connection.createStatement();
-
+            int count = 0;
+            
             String sql;
-            sql = "SELECT * FROM INGEVOERDE_SEQUENTIE";
+            sql = "SELECT MAX (TO_CHAR(ID_SEQUENTIE)) FROM INGEVOERDE_SEQUENTIE";
+            
+//            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+//            Calendar calendar = Calendar.getInstance();
+////            Date date = (Date) calendar.getTime();
+//            java.util.Date utiDate = new java.util.Date();
+//            java.sql.Date sqlDate = new java.sql.Date(utiDate.getTime());
+             
 
             ResultSet rs = stmt.executeQuery(sql);
-
             while (rs.next()) {
-                int id = rs.getInt("ID_SEQUENTIE");
-                String seq = rs.getString("SEQUENTIE");
+                //count = rs.getInt("ID_Sequentie");
+                count = rs.getInt(1);
+            }
+            System.out.println(count);
+            count += 1;
+//invullen database ingevoerde sequentie
+            String query = "INSERT INTO INGEVOERDE_SEQUENTIE (ID_SEQUENTIE, NAAM, DATUM, SEQUENTIE) VALUES (?,?,?,?)"; 
+            PreparedStatement preparedStmt = connection.prepareStatement(query);
+            preparedStmt.setString(1,Integer.toString(count));
+            preparedStmt.setString(2, header);
+            preparedStmt.setDate(3, java.sql.Date.valueOf(java.time.LocalDate.now()));
+            preparedStmt.setString(4, refseq.substring(0, 4000));
 
-                System.out.println("id: " + id);
-                System.out.println("seq: " + seq);
+//            stmt.executeUpdate(query);
+            preparedStmt.execute();
+ // forward sequentie in ORF          
+              for(int i = 0; i<forward.size();i++){
+                String query1 = "INSERT INTO ORF (START_POSITIE, STOP_POSITIE, READING_FRAME, SEQ_ID, SEQUENTIE_ORF)" + 
+                    "VALUES ("+ forward.get(i).getStartPos() +","+ forward.get(i).getStopPos()  +","+  forward.get(i).getORFseq() +","+count +","+forward.get(i).getReadingFrame()+");";
+                stmt.executeUpdate(query1);
+              }
+ // reverse sequentie in oRF
+            for(int i= 0;i<reverse.size();i++){
+                String query2 = "INSERT INTO ORF (START_POSITIE, STOP_POSITIE, READING_FRAME, SEQ_ID, SEQUENTIE_ORF)" +
+                        "VALUES ("+ reverse.get(i).getStartPos()  +","+ reverse.get(i).getStopPos()  +","+ reverse.get(i).getReadingFrame() +","+ count +","+ reverse.get(i).getORFseq()+");";
+            stmt.executeUpdate(query2);
             }
             rs.close();
             stmt.close();
